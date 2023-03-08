b0635e7ad34c62c59f9e38dc98687921cf42de8f
Neh Guk <84685506+NehGuk@users.noreply.github.com>
2023-03-08 22:04:51 +0100

    Updates validation messages in contact form

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -6,19 +6,19 @@ const schema = yup
   .object({
     fullName: yup
       .string()
-      .min(3, 'Your first name should be at least 3 characters.')
+      .min(3, 'Your name should be at least 3 characters.')
       .required('Please enter your first name'),
     email: yup
       .string()
       .email()
-      .required('Please enter your email'),
+      .required('Please enter a valid email address'),
     subject: yup
       .string()
-      .min(3, 'Your first name should be at least 3 characters.')
+      .min(3, 'The subject should be at least 3 characters.')
       .required('Please enter a subject'),
     body: yup
       .string()
-      .min(3, 'Your first name should be at least 3 characters.')
+      .min(3, 'Your message should be at least 3 characters.')
       .max(1000, 'Your first name cannot be longer than 100 characters.')
       .required('Please enter a subject'),
   })

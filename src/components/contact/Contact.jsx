9a1177e1b74442a3d5861c69320870eaab1cc9f6
Neh Guk <84685506+NehGuk@users.noreply.github.com>
2023-03-08 21:36:25 +0100

    Adds form validation with react-hook-form and yup

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,7 +1,65 @@
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
+const schema = yup
+  .object({
+    fullName: yup
+      .string()
+      .min(3, 'Your first name should be at least 3 characters.')
+      .required('Please enter your first name'),
+    email: yup
+      .string()
+      .email()
+      .required('Please enter your email'),
+    subject: yup
+      .string()
+      .min(3, 'Your first name should be at least 3 characters.')
+      .required('Please enter a subject'),
+    body: yup
+      .string()
+      .min(3, 'Your first name should be at least 3 characters.')
+      .max(1000, 'Your first name cannot be longer than 100 characters.')
+      .required('Please enter a subject'),
+  })
+  .required();
+
 export default function Contact() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  function onSubmit(data) {
+    console.log(data);
+  }
+
     return (
       <section>
         <h1>Contact</h1>
+        <form onSubmit={handleSubmit(onSubmit)}>
+
+          <label htmlFor="fullName">Full name</label>
+          <input name="fullName" {...register('fullName')} />
+          <p>{errors.fullName?.message}</p>
+
+          <label htmlFor="email">Email</label>
+          <input name="email" {...register('email')} />
+          <p>{errors.email?.message}</p>
+
+          <label htmlFor="subject">Subject</label>
+          <input name="subject" {...register('subject')} />
+          <p>{errors.subject?.message}</p>
+
+          <label htmlFor="body">Message</label>
+          <input name="body" {...register('body')} />
+          <p>{errors.body?.message}</p>
+
+          <button type="submit">Submit</button>
+        </form>
       </section>
     );
   }

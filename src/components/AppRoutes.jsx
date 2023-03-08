2d67c7c3aa0bb9fd8604ac072edc3d90bc4f8efd
Neh Guk <84685506+NehGuk@users.noreply.github.com>
2023-03-08 14:26:47 +0100

    Adds jsx extensions to AppRoutes module imports

--- src/components/AppRoutes.jsx
+++ src/components/AppRoutes.jsx
@@ -1,11 +1,10 @@
 import { Routes, Route } from "react-router-dom";
 
-import Layout from "../components/Layout";
-import Home from "./home/Home";
-import Product from "./product/Product";
+import Layout from "../components/Layout.jsx";
+import Home from "./home/Home.jsx";
+import Product from "./product/Product.jsx";
 import Cart from "./cart/Cart.jsx";
-import Contact from "./contact/Contact";
-
+import Contact from "./contact/Contact.jsx";
 
 import NotFound from "./not-found/NotFound";
 

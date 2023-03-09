cf01cb4fd5c4aa6781808cb8b42e55ce2bb1e498
Neh Guk <84685506+NehGuk@users.noreply.github.com>
2023-03-09 16:45:47 +0100

    Shows discounted prices and "on sale" label

--- src/components/product/Product.jsx
+++ src/components/product/Product.jsx
@@ -2,14 +2,47 @@ import { Link, useParams } from "react-router-dom";
 import useApi from "../../hooks/APIHook";
 
 export default function Product() {
-  
+  const params = useParams();
+  const { data, isLoading } = useApi(`https://api.noroff.dev/api/v1/online-shop/${params.id}`);
+
+/* Is isLoading is true, we have no data yet. Let's show a loading indicator. */
+if (isLoading) return <div>Loading...</div>
+
+/* testing to see if I can use the data */
+console.log(isLoading);
+console.log(data);
+console.log(data.title);
+console.log(data.tags);
+/* console.log(data.tags.length); */
+console.log(data.reviews);
+/* console.log(data.reviews.length); */
+
+/* Data should exist if we get to here. Let's render the product */
+return (
+  <div>
+    <h1>Product name: {data.title}</h1>
+        <img src={data.imageUrl} />
+        <p>ID: {data.id}</p>
+        <p>{data.discountedPrice === data.price && <p>Price: {data.price}</p>}</p>
+        <p>{data.discountedPrice < data.price && <p>Price: {data.discountedPrice}</p>}</p>
+        <p>{data.discountedPrice < data.price && <p>ON SALE</p>}</p>
+        <p>{data.discountedPrice < data.price && <p>Save {data.price - data.discountedPrice}!</p>}</p>
+        <p>Description: {data.description}</p>
+        <p>Rating: {data.rating}</p>        
+        <div>
+          <Link to="/">Back to products</Link>
+        </div>
+  </div>
+)
+
+/*   
     let params = useParams();
     const product = useApi(`https://api.noroff.dev/api/v1/online-shop/${params.id}`);
     const { id, title, description, price, discountedPrice, imageUrl, rating, tags, reviews } = product.data;
     console.log(reviews);
     console.log(tags);    
-    /* console.log(tags.length); */
-    /* console.log(reviews[0].id); */
+    // console.log(tags.length);
+    // console.log(reviews[0].id);
     
     return (
       <div>
@@ -25,5 +58,5 @@ export default function Product() {
           <Link to="/">Back to products</Link>
         </div>
       </div>
-    );
+    ); */
   }

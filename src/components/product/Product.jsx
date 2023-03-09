1a95b77641803181416646247810ba6c2f489c3f
Neh Guk <84685506+NehGuk@users.noreply.github.com>
2023-03-09 17:09:36 +0100

    Destructures product object

--- src/components/product/Product.jsx
+++ src/components/product/Product.jsx
@@ -4,6 +4,7 @@ import useApi from "../../hooks/APIHook";
 export default function Product() {
   const params = useParams();
   const { data, isLoading } = useApi(`https://api.noroff.dev/api/v1/online-shop/${params.id}`);
+  const { id, title, description, price, discountedPrice, imageUrl, rating, tags, reviews } = data;
 
 /* Is isLoading is true, we have no data yet. Let's show a loading indicator. */
 if (isLoading) return <div>Loading...</div>
@@ -11,24 +12,24 @@ if (isLoading) return <div>Loading...</div>
 /* testing to see if I can use the data */
 console.log(isLoading);
 console.log(data);
-console.log(data.title);
-console.log(data.tags);
+console.log(title);
+console.log(tags);
 /* console.log(data.tags.length); */
-console.log(data.reviews);
+console.log(reviews);
 /* console.log(data.reviews.length); */
 
 /* Data should exist if we get to here. Let's render the product */
 return (
   <section>
-    <h1>{data.title}</h1>
-        <img src={data.imageUrl} />
-        <p>ID: {data.id}</p>
-        <p>{data.discountedPrice === data.price && <p>Price: {data.price}</p>}</p>
-        <p>{data.discountedPrice < data.price && <p>Price: {data.discountedPrice}</p>}</p>
-        <p>{data.discountedPrice < data.price && <p>ON SALE</p>}</p>
-        <p>{data.discountedPrice < data.price && <p>Save {data.price - data.discountedPrice}!</p>}</p>
-        <p>Description: {data.description}</p>
-        <p>Rating: {data.rating}</p>        
+    <h1>{title}</h1>
+        <img src={imageUrl} />
+        <p>ID: {id}</p>
+        <p>{discountedPrice === price && <p>Price: {price}</p>}</p>
+        <p>{discountedPrice < price && <p>Price: {discountedPrice}</p>}</p>
+        <p>{discountedPrice < price && <p>ON SALE</p>}</p>
+        <p>{discountedPrice < price && <p>Save {price - discountedPrice}!</p>}</p>
+        <p>Description: {description}</p>
+        <p>Rating: {rating}</p>        
         <button>Add to cart</button>
         <div>
           <Link to="/">Back to products</Link>

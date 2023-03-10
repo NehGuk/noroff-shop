b80bd47ff19ed7316e3cfcd12fe7bfad825e6062
Neh Guk <84685506+NehGuk@users.noreply.github.com>
2023-03-11 00:38:44 +0100

    Removes product ID from home view

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -19,8 +19,6 @@ export default function Home() {
               <p>{product.price === product.discountedPrice ? <span>Price: {product.price}</span> : <span>Price: {product.discountedPrice}</span>}</p>
               <p>{product.price > product.discountedPrice && <span>ON SALE</span>}</p>
               <p>Rating: {product.rating}</p>
-              <p>ID: {product.id}</p>
-              {/* <p>Test: {product.reviews}</p> */}
             </div>
           ))}
         </div>

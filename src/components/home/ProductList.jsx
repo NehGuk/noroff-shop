72a73195673d58ea6ef1812d42e1b7a53772c132
Neh Guk <84685506+NehGuk@users.noreply.github.com>
2023-03-12 16:42:24 +0100

    Moves products to new ProductList component

---
+++ src/components/home/ProductList.jsx
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom";
+import useApi from "../../hooks/APIHook";
+import showStars from "../../utilities/ReviewStars";
+
+export default function ProductList() {
+    const { data } = useApi("https://api.noroff.dev/api/v1/online-shop");
+    /* console.log(data); */
+    /* console.log("API data fetched"); */
+    
+    return <div>
+    {data.map((product) => (
+      <div key={product.id}>
+        <img src={product.imageUrl} alt={product.title} />
+        <Link to={`/product/${product.id}`}><h2>{product.title}</h2></Link>
+        <p>{product.description}</p>
+        <p>{product.price === product.discountedPrice ? <span>Price: {product.price}</span> : <span>Price: {product.discountedPrice}</span>}</p>
+        <p>{product.price > product.discountedPrice && <span>ON SALE</span>}</p>
+        <p>Rating: {showStars(product.rating)}</p>
+      </div>
+    ))}
+  </div>
+}

f19fb51bda730b42c49a5db1e402aba2a2cd3071
Neh Guk <84685506+NehGuk@users.noreply.github.com>
2023-03-21 09:24:06 +0100

    Adds sample addToCart function to test the button

--- src/components/product/Product.jsx
+++ src/components/product/Product.jsx
@@ -6,12 +6,19 @@ import showStars from "../../utilities/ReviewStars";
 export default function Product() {
   const params = useParams();
   const { data, isLoading } = useApi(`https://api.noroff.dev/api/v1/online-shop/${params.id}`);
-  const { title, description, price, discountedPrice, imageUrl, rating, tags, reviews } = data;
+  const { title, id, description, price, discountedPrice, imageUrl, rating, tags, reviews } = data;
   
   /* Is isLoading is true, we have no data yet. Let's show a loading indicator. */
   if (isLoading) return <div>Loading...</div>
   
   /* Data should exist if we get to here. Let's render the product */
+  function addToCart() {
+    console.log("Added to cart:");
+    console.log(title);
+    console.log(price);
+    console.log(id);
+  }
+
   return (
     <section>
       <h1>{title}</h1>
@@ -23,7 +30,7 @@ export default function Product() {
           <p>Description: {description}</p>
           <p>{showStars(rating)} <span> ({rating})</span></p>        
           <p>Tags: {tags && <span>{tags.join(' ')}</span>}</p>
-          <button>Add to cart</button>
+          <button onClick={addToCart}>Add to cart</button>
           <p><Link to="/">Back to products</Link></p>
           {reviews && <div><Reviews reviews={reviews}/></div>}       
     </section>

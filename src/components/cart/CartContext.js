a45fe38c2e57feef28555f77ff6366bb5df2a944
Neh Guk <84685506+NehGuk@users.noreply.github.com>
2023-03-23 18:31:10 +0100

    Removes test variable

--- src/components/cart/CartContext.js
+++ src/components/cart/CartContext.js
@@ -6,7 +6,6 @@ export const CartContext = createContext();
 export default function CartContextProvider(props) {
   /* const [cartItems, setCartItems] = useState([]); */
   const [cartItems, setCartItems] = useLocalStorage("cart", []);
-  const testingVariable = "Transferred variable via Context!!! OKKKKK";
 
   function addToCart(item) {
     setCartItems((prevItems) => [...prevItems, item]);
@@ -24,5 +23,5 @@ export default function CartContextProvider(props) {
     setCartItems([]);
   }
 
-  return <CartContext.Provider value={{ cartItems, addToCart, removeFromCart, clearCart, testingVariable }}>{props.children}</CartContext.Provider>;
+  return <CartContext.Provider value={{ cartItems, addToCart, removeFromCart, clearCart }}>{props.children}</CartContext.Provider>;
 }

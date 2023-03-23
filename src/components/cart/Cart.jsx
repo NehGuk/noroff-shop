4968de4503330b9a3ef111f31ea15f7620ad74ed
Neh Guk <84685506+NehGuk@users.noreply.github.com>
2023-03-23 18:31:32 +0100

    Adds data to cart component

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -1,18 +1,30 @@
 import { Link } from "react-router-dom";
 import { useContext } from "react";
 import { CartContext } from "./CartContext";
+import countEachCartItem from "../../utilities/cartItemsCount";
 
 export default function Cart() {
-    const {cartItems, addToCart, RemoveFromCart, clearCart, testingVariable} = useContext(CartContext);
+    const {cartItems, addToCart, RemoveFromCart, clearCart} = useContext(CartContext);
     console.log(cartItems);
+    
     return <section>
             <h1>My cart</h1>
-            <p>Cart content: {testingVariable}</p>
-            <div>
-            <button onClick={clearCart}>Clear cart</button>
-            </div>
+            <p>Items in the cart: {cartItems.length}</p>
+            {cartItems.map((itemAdded) => (
+                <div>
+                    <div>{itemAdded.title}</div>
+                    <div>{itemAdded.price}</div>
+                    <div>Quantity: X</div>
+                </div>
+            ))}
             
-            <Link to="/checkout">Checkout</Link>
+            <div>
+            
+            </div>
+            <div>Total: YYY</div>
+            <button onClick={clearCart}>Clear cart</button>
+            <div><Link to="/checkout">Checkout</Link></div>
+            
         </section>
 }
 

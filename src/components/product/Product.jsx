34e9b5ccb0882dfbb2d0f9cd331cad8e4be6baa1
Neh Guk <84685506+NehGuk@users.noreply.github.com>
2023-03-24 16:47:54 +0100

    Refactors the code for cart add and remove buttons

--- src/components/product/Product.jsx
+++ src/components/product/Product.jsx
@@ -1,21 +1,38 @@
 import { Link, useParams } from "react-router-dom";
+import { useState } from "react";
 import useApi from "../../hooks/APIHook";
 import Reviews from "./Reviews";
 import showStars from "../../utilities/ReviewStars";
 import { useContext } from "react";
 import { CartContext } from "../context/CartContext";
 
+
 export default function Product() {
   const params = useParams();
   const { data, isLoading } = useApi(`https://api.noroff.dev/api/v1/online-shop/${params.id}`);
   const { title, description, price, discountedPrice, imageUrl, rating, tags, reviews } = data;
-  
   const { cartItems, addToCart, removeFromCart } = useContext(CartContext);
-  /* Is isLoading is true, we have no data yet. Let's show a loading indicator. */
+  const [addButton, setAddButton] = useState(true);
+  const [removeButton, setRemoveButton] = useState(false);
+  const alreadyInCart = cartItems.find(item => item.id === data.id);
+
+  console.log("IS IT IN THE CART?????");
+  {alreadyInCart ? console.log("ALREADY IN CARTTTTT YESSSS") : console.log("NOT IN CARTTT OHHH NOOOOOO");}
+    
   if (isLoading) return <div>Loading...</div>
   
+  function handleAddButton() {
+    setAddButton(false);
+    setRemoveButton(true);
+    addToCart(data);
+  }
 
-  /* Data should exist if we get to here. Let's render the product */
+  function handleRemoveButton() {
+    setRemoveButton(false);
+    setAddButton(true);
+    removeFromCart(data);
+  }
+  
   return (
     <section>
       <h1>{title}</h1>
@@ -28,20 +45,16 @@ export default function Product() {
           <p>Description: {description}</p>
           <p>{showStars(rating)} <span> ({rating})</span></p>        
           <p>Tags: {tags && <span>{tags.join(' ')}</span>}</p>
-          <button onClick={() => addToCart(data)}>Add to cart</button>
-          <button onClick={() => removeFromCart(data)}>Remove from cart</button>
-
+    
+          <div>
+            <button onClick={handleAddButton} style={{ display: addButton ? 'block' : 'none' }}>Add</button>
+            <button onClick={handleRemoveButton} style={{ display: removeButton ? 'block' : 'none' }}>Remove</button>
+          </div>
+          
           {cartItems.includes(description) && console.log("A lot of items here")}
           
-          
-
-
-
-          {/* <button onClick={() => removeFromCart(data)}>Remove from cart</button> */}
           <p><Link to="/">Back to products</Link></p>
           {reviews && <div><Reviews reviews={reviews}/></div>}       
     </section>
   )
 }
-
-// if the array cartItems contains a product (object) with this ID, show the "Remove from card" button.

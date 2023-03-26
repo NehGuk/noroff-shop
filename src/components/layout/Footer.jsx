279d51ce6994b83113b8ee72c32147177ff6a98b
Neh Guk <84685506+NehGuk@users.noreply.github.com>
2023-03-26 20:42:59 +0200

    Updates footer structure

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,3 +1,18 @@
 export default function Footer() {
-    return <footer>Footer</footer>;
+    return <footer>
+        <div>
+          <h3>Footer</h3>
+        </div>
+        <div>
+          <p>Javascript Frameworks Course Assignment</p>
+          <p>2023</p>
+        </div>
+        <div>
+          <h3>Built with</h3>
+            <ul>
+              <li>React</li>
+              <li>Noroff API</li>
+            </ul>
+        </div>
+      </footer>;
   }

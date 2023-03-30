4c61e3b33577cab85a4d21af48ba9f0962479e5d
Neh Guk <84685506+NehGuk@users.noreply.github.com>
2023-03-30 20:24:44 +0200

    Styles h1 and Search

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,15 +1,19 @@
 import Search from "./search/Search";
 /* import ProductList from "./ProductList";
 import NoResults from "./search/NoResults"; */
-import { HomeSection } from "./Home.style";
+import { HomeSection, Welcome } from "./Home.style";
 
 export default function Home() {  
     return (
       <HomeSection>
-        <div><h1>Home</h1></div>
-        {<div><Search /></div>}
-      
-        
+        <Welcome>
+          <h1>Welcome to the Noroff API Shop!</h1>
+          
+        </Welcome>
+        <Search />
+
+        {/* <div><h1>Welcome</h1></div>
+        <div><Search /></div> */}
         
         {/* {<ProductList /> && !<NoResults />}  */}
         </HomeSection>
--- src/components/home/search/Search.style.jsx
+++ src/components/home/search/Search.style.jsx
@@ -1,4 +1,28 @@
 import styled from "styled-components";
 
 export const SearchArea = styled.div`
+/* text-align: center; */
+`;
+
+export const SearchInputArea = styled.div`
+
+
+    text-align: center;
+    > input {
+        border: none;
+        padding: .7em 2em;
+        font-size: large;
+        border-radius: .2em;
+        box-shadow: 0 0 15px 5px ${(props) => props.theme.color.primary};
+        outline: none;
+        transition: ease-in-out .2s;
+        :focus {
+            box-shadow: 0 0 15px 10px ${(props) => props.theme.color.primary};
+            
+            transition: ease-in-out .2s;
+
+        }
+        
+    }
+
 `;

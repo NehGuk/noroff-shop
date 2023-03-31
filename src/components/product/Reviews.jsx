a51b24b5764ad96a3402bd26e607c0fa9ebdb676
Neh Guk <84685506+NehGuk@users.noreply.github.com>
2023-03-31 10:09:09 +0200

    Adds conditional for "no reviews"

--- src/components/product/Reviews.jsx
+++ src/components/product/Reviews.jsx
@@ -4,6 +4,7 @@ export default function Reviews(props) {
     const { reviews: reviewsArray } = props;
     return <div>
         <h2>Reviews</h2>
+        {reviewsArray.length === 0 && <div>This product has no reviews yet.</div>}
         {reviewsArray.map((review) => (<div key={review.id}>
             <hr></hr>
             <p>Rating: {showStars(review.rating, 16)}</p>
